7095c9808b035f6abca4a4c350f65ce8b0f87be1
holy5 <r.magine5@gmail.com>
2021-10-28 20:15:40 +0700

    add slider and finish up

--- js/app.js
+++ js/app.js
@@ -15,3 +15,23 @@ window.addEventListener("load", function () {
     this.classList.remove("is-active");
   });
 });
+$(".review-list").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow:
+    "<button type='button' class='slick-prev slick-arrow'><i class='fal fa-long-arrow-left' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next slick-arrow'><i class='fal fa-long-arrow-right' aria-hidden='true'></i></button>",
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      },
+    },
+  ],
+});
